3711d143bc3b49873925ef087314c966db946cb3
AceAnson <s2012002@lkcss.edu.hk>
2016-11-24 15:43:46 +0800

    congratulations

---
+++ sudoku2/app/src/main/java/com/example/s2012002/sudoku/MainActivity01.java
@@ -0,0 +1,13 @@
+package com.example.s2012002.sudoku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
